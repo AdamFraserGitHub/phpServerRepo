d7f78d8544c874c17babe46c8aaaaa68f2bdba91
Adam Fraser AHCS <adamfraserahcs@gmail.com>
2019-01-14 09:29:45 +0000

    c

--- database1.php
+++ database1.php
@@ -6,10 +6,12 @@
     
     $db_conn = new mysqli($host, $username, $dbpass, $db);
     
-    if($result = ($db_conn->query("SELECT * FROM table0"))) {
-        echo "ok";
-    } else {
+    if($result = ($db_conn->query("SELECT * FROM table0"))) {} 
+    else {
         echo "well shit";
+        die;
     }
+
+    $query = "INSERT INTO table0 (name, age) VALUES ('adam', 16)";
 
 ?>
